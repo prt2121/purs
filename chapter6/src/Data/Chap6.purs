f9c356b3882fc0db384afe2df60c0eff8a569967
Prat Tanapaisankit <prat@meetup.com>
2016-03-23 20:55:26 -0400

    Add funtor and foldable instance

--- chapter6/src/Data/Chap6.purs
+++ chapter6/src/Data/Chap6.purs
@@ -31,3 +31,26 @@ instance semiGroupNonEmpty :: Semigroup (NonEmpty a) where
   -- > import Chap6
   -- > append (NonEmpty 1.0 [2.0,3.0]) (NonEmpty 7.0 [9.0])
   -- [ h 1.0, t [2.0,3.0,7.0,9.0]]
+
+-- class Functor f where
+--   map :: forall a b. (a -> b) -> f a -> f b
+instance functorNonEmpty :: Functor NonEmpty where
+  map f (NonEmpty x xs) = NonEmpty (f x) $ map f xs
+
+  -- map (* 2.0) $ NonEmpty 1.0 [2.0, 3.0]
+  -- [ h 2.0, t [4.0,6.0]]
+
+-- class Foldable f where
+--   foldr :: forall a b. (a -> b -> b) -> b -> f a -> b
+--   foldl :: forall a b. (b -> a -> b) -> b -> f a -> b
+--   foldMap :: forall a m. (Monoid m) => (a -> m) -> f a -> m
+instance foldableNonEmpty :: Foldable NonEmpty where
+  foldr f x xs = foldr f x (toArray xs)
+  foldl f x xs = foldl f x (toArray xs)
+  foldMap f xs = foldMap f (toArray xs)
+
+-- foldl (\x y -> x * y) 4.0 (NonEmpty 1.0 [2.0, 3.0])
+-- 24.0
+
+toArray :: forall a. NonEmpty a -> Array a
+toArray (NonEmpty a as) = a : as

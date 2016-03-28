4a16bb2b7238fba3839813b0ed308e71b86a50f7
Prat <banqfirn@gmail.com>
2016-03-27 23:04:00 -0400

    Add liftedPlus

---
+++ chapter7/src/Main.purs
@@ -0,0 +1,26 @@
+module Main where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Apply
+import Data.Maybe
+
+liftedPlus :: forall a. (Semiring a) => Maybe a -> Maybe a -> Maybe a
+liftedPlus = lift2 (+)
+
+-- import Control.Apply
+-- import Prelude
+-- import Data.Maybe
+-- liftedPlus :: Maybe Int
+-- liftedPlus = lift2 (+) 5 2
+-- liftedPlus x y = lift2 (+) x y
+-- Just (5)
+
+main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = do
+  log "Hello sailor!"
+
+  -- import Data.Maybe
+  -- liftedPlus (Just 2) (Just 3)
+  -- Just (5)

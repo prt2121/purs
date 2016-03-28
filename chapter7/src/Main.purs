7aa6a27cb31854e500a529f1dba825a5763bb9a1
Prat Tanapaisankit <prat@meetup.com>
2016-03-28 07:53:22 -0400

    Add maybeMul maybeDiv

--- chapter7/src/Main.purs
+++ chapter7/src/Main.purs
@@ -5,6 +5,7 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Apply
 import Data.Maybe
+import Math
 
 maybePlus :: forall a. (Semiring a) => Maybe a -> Maybe a -> Maybe a
 maybePlus = lift2 (+)
@@ -16,6 +17,18 @@ maybePlus = lift2 (+)
 -- liftedPlus = lift2 (+) 5 2
 -- liftedPlus x y = lift2 (+) x y
 -- Just (5)
+
+maybeMinus :: forall a. (Ring a) => Maybe a -> Maybe a -> Maybe a
+maybeMinus = lift2 (-)
+
+maybeMul :: forall a. (Semiring a) => Maybe a -> Maybe a -> Maybe a
+maybeMul = lift2 (*)
+
+maybeDiv :: forall a. (ModuloSemiring a) => Maybe a -> Maybe a -> Maybe a
+maybeDiv = lift2 (/)
+
+-- forall a f. (Applicativef) => Maybe (f a) -> f (Maybe a)
+
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do

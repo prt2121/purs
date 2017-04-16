483eb390c0433f26753dfadc1dd622905e290bbd
prat t <banqfirn@gmail.com>
2017-04-16 15:37:03 -0400

    add simple repl with Free & Aff

--- free-play/src/Main.purs
+++ free-play/src/Main.purs
@@ -1,21 +1,18 @@
 module Main where
 
 import Prelude
-import Control.Monad
-import Control.Monad.Free
-import Control.Monad.Free.Trans
-import Control.Monad.Eff.Class
-import Control.Monad.Reader.Class
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Control.Monad.Reader.Trans (ReaderT(..), runReaderT)
+import Control.Monad.Free.Trans (FreeT, hoistFreeT, liftFreeT, runFreeT)
 import Data.Identity (Identity)
 import Data.Newtype (unwrap)
-import Node.ReadLine (Interface, READLINE, close, createConsoleInterface, noCompletion, prompt, setLineHandler, setPrompt)
+import Node.ReadLine (READLINE)
+import Node.SimpleRepl (Repl, putStrLn, readLine, runRepl)
 
 type CmdEff eff = Eff (readline :: READLINE, console :: CONSOLE, exception :: EXCEPTION | eff)
-type Repl e a = ReaderT Interface (CmdEff e) a
+type CmdT       = FreeT CmdF
+type Cmd        = CmdT Identity
 
 data CmdF a =
   PrintStr String a
@@ -24,9 +21,6 @@ data CmdF a =
 instance cmdF :: Functor CmdF where
   map f (PrintStr s x) = PrintStr s (f x)
   map f (GetStr x)     = GetStr (x >>> f)
-
-type CmdT = FreeT CmdF
-type Cmd = CmdT Identity
 
 liftCmd :: ∀ m
            . Monad m
@@ -47,22 +41,26 @@ getStr = liftCmd $ GetStr id
 
 interpretCmd :: ∀ eff
                 . CmdF
-                ~> CmdEff eff
+                ~> Repl eff
 interpretCmd = go where
   go (PrintStr s a) =
-    const a <$> log s
-  go (GetStr k) = k <$> pure "test"
+    const a <$> putStrLn s
+  go (GetStr k) = k <$> readLine
 
 runCmd :: ∀ eff
           . Cmd
-          ~> CmdEff eff
+          ~> Repl eff
 runCmd =
   runFreeT interpretCmd <<< hoistFreeT (pure <<< unwrap)
 
 main :: ∀ eff
         . CmdEff eff Unit
-main = do
-  runCmd $ do
-    printStr "blah"
-    str <- getStr
-    printStr str
+main =
+  runRepl do
+    loop
+    where
+      loop = do
+        runCmd $ do
+          str <- getStr
+          printStr str
+        loop
